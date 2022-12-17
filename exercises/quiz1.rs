b83965f329e089edaf516fe76fa45d330ca66a7d
chan <channyein.k@gmail.com>
2022-12-18 01:27:50 +0700

    exercise: finished condition if and primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple: i32) -> i32 {
+    if apple > 40 {
+        apple
+    } else {
+        apple * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
